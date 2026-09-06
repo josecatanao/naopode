006124e56f07c805fea7291e4bbb4363b3698981
josé neto <josecatanaoneto@gmail.com>
2026-09-06 17:51:43 -0300

    Fix Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.NAO_PODE_SUPABASE = {
-  url: "https://fzwqepnsxiijpyrswekn.supabase.co",
+  url: "https://fzwqepnsxiiypqrswekn.supabase.co",
   publishableKey: "sb_publishable_Jyh8bM9g9mnT0INVOCXTDg_SlgnT2Bn",
   publicAppUrl: "https://josecatanao.github.io/naopode/"
 };
