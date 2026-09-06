fe9b9cf778ffde6a8869b3ad257362c329599dd2
josé neto <josecatanaoneto@gmail.com>
2026-09-06 17:36:07 -0300

    Configure GitHub Pages app URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.NAO_PODE_SUPABASE = {
   url: "https://fzwqepnsxiijpyrswekn.supabase.co",
   publishableKey: "sb_publishable_Jyh8bM9g9mnT0INVOCXTDg_SlgnT2Bn",
-  publicAppUrl: ""
+  publicAppUrl: "https://josecatanao.github.io/naopode/"
 };
